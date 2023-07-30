31846260bab61903dc1f1e2a44fde33aa59d4095
VladBorod <jazgigantsmith@gmail.com>
2023-07-30 14:43:56 +0300

    Next three forks of algorithm DONE

--- choose_funcs.py
+++ choose_funcs.py
@@ -1,10 +1,13 @@
 from text_for_user import *
 
-body_weight = float(0)
+body_weight = float(1.5)
 
 
 def sultasini_calculation(weight):
     """Функция расчета сультасина"""
     global body_weight
     result = body_weight * 75
-    return result, sultasini_message
+    return result, text_sultasini_message
+
+
+sul = sultasini_calculation(body_weight)
--- medic_force.py
+++ medic_force.py
@@ -19,15 +19,20 @@ def start(message):
                                       f'давай рассчитаем антибиотики, </b>',
                      parse_mode='html')
     # Ширина ряда добавления кнопок.
-    markup = types.InlineKeyboardMarkup(row_width=3)
+    markup = types.InlineKeyboardMarkup(row_width=2)
     # Новорожденные с ДН без факторов.
-    newborn_respiratory_no_risk = types.InlineKeyboardButton(button_newborn_respiratory_no_risk, callback_data='data1')
+    newborn_respiratory_no_risk = types.InlineKeyboardButton(text_button_newborn_respiratory_no_risk,
+                                                             callback_data='fork_f_t_risk_f_rds')
     # НР без КС, но с условными ФР.
-    newborn_no_clinic_conditional = types.InlineKeyboardButton(button_newborn_no_clinic_conditional, callback_data='data2')
+    newborn_no_clinic_conditional = types.InlineKeyboardButton(text_button_newborn_no_clinic_conditional,
+                                                               callback_data='fork_f_conditional')
     # НР без КС, но с абсолютными ФР.
-    newborn_no_clinic_absolute = types.InlineKeyboardButton(button_newborn_no_clinic_absolute, callback_data='data3')
-    # Добавление кнопок в ряд шириной 3.
-    markup.add(newborn_respiratory_no_risk, newborn_no_clinic_conditional,
+    newborn_no_clinic_absolute = types.InlineKeyboardButton(text_button_newborn_no_clinic_absolute,
+                                                            callback_data='fork_f_absolute')
+    # Добавление верхней кнопки.
+    markup.row(newborn_respiratory_no_risk)
+    # Добавление двух нижних кнопок.
+    markup.add(newborn_no_clinic_conditional,
                newborn_no_clinic_absolute)
     # Сообщение о факторах риска.
     bot.send_message(message.chat.id, text_risc_factors, parse_mode='HTML')
@@ -37,23 +42,38 @@ def start(message):
     bot.send_message(message.chat.id, text_variant_choose, parse_mode='HTML', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda callback: True)
-def callback_message(callback):
-    if callback.data == 'data1':
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        more_than_two_risk_factors_rds = types.InlineKeyboardButton(button_more_than_two_risc_factors_rds)
-        less_than_two_risk_factors_rds = types.InlineKeyboardButton(button_more_than_two_risc_factors_rds)
-        markup.add(more_than_two_risk_factors_rds)
-        markup.add(less_than_two_risk_factors_rds)
-        bot.send_message(callback.message.chat.id, 'Whats next')
-        # less_than_two_risk_factors_rds = types.KeyboardButton(button_less_than_two_risc_factors_rds)
-        # markup.add(more_than_two_risk_factors_rds, less_than_two_risk_factors_rds)
-        # restart = types.KeyboardButton(button_restart_text)
-        # markup.row(restart, callback_data='/start')
-    # else:
-    #     markup = types.ReplyKeyboardMarkup(row_width=1)
-    #     restart = types.KeyboardButton(button_restart_text)
-    #     markup.add(restart, callback_data='/start')
+@bot.callback_query_handler(func=lambda callback: callback.data)
+def first_fork(callback):
+    if callback.data == 'fork_f_t_risk_f_rds':
+        markup_rds = types.InlineKeyboardMarkup(row_width=1)
+        more_than_two_risk_factors_rds = types.InlineKeyboardButton(text_button_more_than_two_risc_factors_rds,
+                                                                    callback_data='fork_f_more_t_r_f_rds')
+        less_than_two_risk_factors_rds = types.InlineKeyboardButton(text_button_less_than_two_risc_factors_rds,
+                                                                    callback_data='fork_f_less_t_r_f_rds')
+        markup_rds.add(more_than_two_risk_factors_rds, less_than_two_risk_factors_rds)
+        bot.send_message(callback.message.chat.id, text_fork_for_more_and_less_than_two_risk_factors,
+                         parse_mode='HTML', reply_markup=markup_rds)
+    elif callback.data == 'fork_f_conditional':
+        markup_conditional = types.InlineKeyboardMarkup(row_width=1)
+        gestational_age_more_35 = types.InlineKeyboardButton(text_button_more_35_w_g_a,
+                                                             callback_data='fork_f_more_t_35_w_g_a')
+        gestational_age_less_35 = types.InlineKeyboardButton(text_button_less_35_w_g_a,
+                                                             callback_data='fork_f_less_t_35_w_g_a')
+        markup_conditional.add(gestational_age_more_35, gestational_age_less_35)
+        bot.send_message(callback.message.chat.id, text_for_gestational_age_choose,
+                         parse_mode='HTML', reply_markup=markup_conditional)
+    elif callback.data == 'fork_f_absolute':
+        markup_absolute = types.InlineKeyboardMarkup(row_width=1)
+        more_than_two_risk_factors_absolute = types.InlineKeyboardButton(
+            text_button_more_than_two_risc_factors_absolute,
+            callback_data='fork_f_more_t_2_risc_f_absolute')
+        less_than_two_risk_factors_absolute = types.InlineKeyboardButton(
+            text_button_less_than_two_risc_factors_absolute,
+            callback_data='fork_f_less_t_2_risc_f_absolute')
+        markup_absolute.add(more_than_two_risk_factors_absolute, less_than_two_risk_factors_absolute)
+        bot.send_message(callback.message.chat.id, text_fork_for_more_and_less_than_two_risk_factors,
+                         parse_mode='HTML', reply_markup=markup_absolute)
+
 
 
 # Постоянная работа бота.
